1789bddc583752da69aeeef9eb3708f56abc3f22
Duvan <duvanriano2626@gmail.com>
2024-07-08 17:32:54 -0500

    Funcionanmiento de la barra de busqueda

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,6 +6,17 @@ import { Component, Input } from '@angular/core';
   styleUrl: './search-box.component.css'
 })
 export class SearchBoxComponent {
+  @ViewChild('txtInput')
+  public Input!: ElementRef <HTMLInputElement>;
+
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+
+  public emitCapital = (): void => {
+    this.onValue.emit(this.Input.nativeElement.value);
+  }
 }
