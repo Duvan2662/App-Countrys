7f2b67869e5a8778df4b441888c17d00308eef44
Duvan <duvanriano2626@gmail.com>
2024-07-08 15:28:37 -0500

    Rutas de mis paginas importante

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPagesComponent
+    AboutPagesComponent,
+    SideBarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HomePageComponent,
-    AboutPagesComponent
+    AboutPagesComponent,
+    ContactPageComponent,
+    SideBarComponent
   ]
 })
 export class SharedModule { }
