9a01f3187840e762e5a0465f29bd1c21525547ff
Duvan <duvanriano2626@gmail.com>
2024-07-08 19:12:12 -0500

    Creacion de la tabla donde se va a presentar la informacion

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -7,6 +7,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { CountriesService } from './services/countries.service';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -15,7 +16,8 @@ import { CountriesService } from './services/countries.service';
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
